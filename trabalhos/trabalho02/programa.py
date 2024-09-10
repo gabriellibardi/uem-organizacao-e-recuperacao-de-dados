8bcb62b40c146b58c5c670da09ab40ee0a95e3c5
Gabriel Libardi <gabriellibardilulu@gmail.com>
2024-09-10 10:38:33 -0300

    Operação de inserção de registros no trabalho 02

--- trabalhos/trabalho02/programa.py
+++ trabalhos/trabalho02/programa.py
@@ -1,8 +1,9 @@
 import sys
 import io
 import arvoreb
+from os import SEEK_END
 
-NOME_ARQ_DADOS : str = 'games.dat' # Nome do arquivo de registros
+NOME_ARQ_DADOS : str = 'games20.dat' # Nome do arquivo de registros
 NOME_ARQ_SAIDA : str = 'btree.dat' # Nome do arquivo de armazenamento do índice
 TAM_CABECALHO: int = 4 # Quantidade de bytes que o arquivo de registros usa para armazenar o cabeçalho
 TAM_TAM_REGISTRO: int = 2 # Quantidade de bytes que o arquivo de registros usa para armazenar o tamanho dos registros
@@ -94,7 +95,7 @@ def le_operacoes():
         quit()
 
     try:
-        arq_dados = open(NOME_ARQ_DADOS, 'rb')
+        arq_dados = open(NOME_ARQ_DADOS, 'r+b')
     except:
         print('Arquivo de dados não encontrado.')
         quit()
@@ -107,13 +108,17 @@ def le_operacoes():
             executa_busca(arq_arvore, arq_dados, int(arg))
             print()
         elif operacao == 'i':
-            #arvoreb.insere_na_arvore()
-            print('insercao\n')
+            executa_insercao(arq_arvore, arq_dados, arg)
+            print()
         else:
             print('Operação inválida.')
         linha = (arq_operacoes.readline()).strip()
 
-def executa_busca(arq_arvore: io.BufferedRandom, arq_dados: io.BufferedReader, chave: int):
+    arq_operacoes.close()
+    arq_arvore.close()
+    arq_dados.close()
+
+def executa_busca(arq_arvore: io.BufferedRandom, arq_dados: io.BufferedRandom, chave: int):
     '''
     Executa a busca de um registro no índice por meio de sua chave
     '''
@@ -125,9 +130,46 @@ def executa_busca(arq_arvore: io.BufferedRandom, arq_dados: io.BufferedReader, c
         pagina = arvoreb.le_pagina(arq_arvore, rrn)
         arq_dados.seek(pagina.byteoffsets[posicao])
         tam_registro = int.from_bytes(arq_dados.read(TAM_TAM_REGISTRO), signed=True, byteorder='little')
-        print((arq_dados.read(tam_registro)).decode())
+        print((arq_dados.read(tam_registro)).decode() + \
+               ' (' + str(tam_registro) + ' bytes - offset ' + str(pagina.byteoffsets[posicao]) + ')')
     else:
         print('ERRO: registro não encontrado!')
+
+def executa_insercao(arq_arvore: io.BufferedRandom, arq_dados: io.BufferedRandom, registro: str):
+    '''
+    Executa a insercao do registro no arquivo de índices e de registros
+    '''
+    chave = registro[:registro.find('|')].strip()
+    arq_arvore.seek(0)
+    raiz = int.from_bytes(arq_arvore.read(arvoreb.TAM_CABECALHO), signed=True, byteorder='little')
+    print('Inserção do registro de chave "' + chave + '"')
+    achou = arvoreb.busca_na_arvore(arq_arvore, int(chave), raiz)[0]
+    if achou:
+        print('ERRO: chave "' + chave + '" já existe!')
+    else:
+        byteoffset, tam_registro = insere_registro(arq_dados, registro)
+        arvoreb.insere_chave(arq_arvore, int(chave), byteoffset)
+        print(registro + ' (' + str(tam_registro) + ' bytes - offset ' + str(byteoffset) + ')')
+
+def insere_registro(arq_dados: io.BufferedRandom, registro: str) -> tuple[int, int]:
+    '''
+    Insere o registro no fim do arquivo de registros, retornando seu byteoffset e tamanho
+    '''
+    # Adiciona 1 na quantidade de registros
+    arq_dados.seek(0)
+    qnt_registros = int.from_bytes(arq_dados.read(TAM_CABECALHO), signed=True, byteorder='little')
+    qnt_registros += 1
+    arq_dados.seek(0)
+    arq_dados.write(qnt_registros.to_bytes(TAM_CABECALHO, signed=True, byteorder='little'))
+
+    # Escreve o registro no fim do arquivo
+    arq_dados.seek(0, SEEK_END)
+    byteoffset = arq_dados.tell()
+    tam_registro = len(registro)
+    arq_dados.write(tam_registro.to_bytes(TAM_TAM_REGISTRO, signed=True, byteorder='little'))
+    arq_dados.write(registro.encode())
+
+    return byteoffset, tam_registro
 
 def imprime_indice():
     '''
